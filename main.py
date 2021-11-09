800c239804926e49054a778bca01e0a65c5eff22
cmountjoy <cassidy.mountjoy@blockpoint.systems>
2021-11-09 14:44:11 -0800

    removed old code, updated comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# This is a sample Python script.
+# This is a python script for streaming Ethereum blocks to the bSQL database
 from web3 import Web3
 import mdb_bp
 import asyncio
 import datetime
 
-infru = "https://mainnet.infura.io/v3/50f9f82ce07e451c9abe4cd40d72cd60"
+# Globally define our connections to Infura and bSQL
+infru = "https://mainnet.infura.io/v3/your_project_id"
 web3 = Web3(Web3.HTTPProvider(infru))
 conn = mdb_bp.driver.connect(
     username="your username",
@@ -69,86 +70,3 @@ def main():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-    # import time
-    # import datetime
-    #
-    # infru = "https://mainnet.infura.io/v3/50f9f82ce07e451c9abe4cd40d72cd60"
-    # web3 = Web3(Web3.HTTPProvider(infru))
-    # print(web3.isConnected())
-    # block = web3.eth.get_block('latest')
-    # print(str(block['number']))
-    # conn = mdb_bp.driver.connect(
-    #     username="system",
-    #     password="biglove",
-    #     connection_protocol="tcp",
-    #     server_address="localhost",
-    #     server_port=5461,
-    #     database_name="eth",
-    #     parameters={"interpolateParams": True},
-    # )
-    # rows = conn.query("SELECT * FROM blocks")
-    # itr = iter(rows)
-    # for row in itr:
-    #     print(row)
-    #
-    # # date = datetime.datetime.utcfromtimestamp(block['timestamp'])
-    # # print(str(date))
-    # # print(str(block['number']))
-    # # print(str(block['hash'].hex()))
-    # # print(str(block['parentHash'].hex()))
-    # # print(str(block['nonce'].hex()))
-    # # print(str(block['sha3Uncles'].hex()))
-    # # print(str(block['logsBloom'].hex()))
-    # # print(str(block['transactionsRoot'].hex()))
-    # # print(str(block['stateRoot'].hex()))
-    # # print(str(block['receiptsRoot'].hex()))
-    # # print(str(block['miner']))
-    # # print(str(block['difficulty']))
-    # # print(str(block['size']))
-    # # print(str(block['extraData'].hex()))
-    # # print(str(block['gasLimit']))
-    # # print(str(block['baseFeePerGas']))
-    # # print(str(len(block['transactions'])))
-    # # print(str(block['baseFeePerGas']))
-    #
-    # # print("INSERT blocks VALUES (" +
-    # #       "\"" + str(datetime.datetime.utcfromtimestamp(block['timestamp'])) + "\"," +
-    # #       str(block['number']) + "," +
-    # #       "\"" + str(block['hash'].hex()) + "\"," +
-    # #       "\"" + str(block['parentHash'].hex()) + "\"," +
-    # #       "\"" + str(block['nonce'].hex()) + "\"," +
-    # #       "\"" + str(block['sha3Uncles'].hex()) + "\"," +
-    # #       "\"" + str(block['logsBloom'].hex()) + "\"," +
-    # #       "\"" + str(block['transactionsRoot'].hex()) + "\"," +
-    # #       "\"" + str(block['stateRoot'].hex()) + "\"," +
-    # #       "\"" + str(block['receiptsRoot'].hex()) + "\"," +
-    # #       "\"" + str(block['miner']) + "\"," +
-    # #       str(block['difficulty']) + "," +
-    # #       str(block['size']) + "," +
-    # #       "\"" + str(block['extraData'].hex()) + "\"," +
-    # #       str(block['gasLimit']) + "," +
-    # #       str(block['baseFeePerGas']) + "," +
-    # #       str(len(block['transactions'])) + "," +
-    # #       str(block['baseFeePerGas']) + ")")
-    #
-    # conn.exec("INSERT blocks VALUES (" +
-    #       "\"" + str(datetime.datetime.utcfromtimestamp(block['timestamp'])) + "\"," +
-    #       str(block['number']) + "," +
-    #       "\"" + str(block['hash'].hex()) + "\"," +
-    #       "\"" + str(block['parentHash'].hex()) + "\"," +
-    #       "\"" + str(block['nonce'].hex()) + "\"," +
-    #       "\"" + str(block['sha3Uncles'].hex()) + "\"," +
-    #       "\"" + str(block['logsBloom'].hex()) + "\"," +
-    #       "\"" + str(block['transactionsRoot'].hex()) + "\"," +
-    #       "\"" + str(block['stateRoot'].hex()) + "\"," +
-    #       "\"" + str(block['receiptsRoot'].hex()) + "\"," +
-    #       "\"" + str(block['miner']) + "\"," +
-    #       str(block['difficulty']) + "," +
-    #       str(block['size']) + "," +
-    #       "\"" + str(block['extraData'].hex()) + "\"," +
-    #       str(block['gasLimit']) + "," +
-    #       str(len(block['transactions'])) + "," +
-    #       str(block['baseFeePerGas']) + ")")
-    # conn.close()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
